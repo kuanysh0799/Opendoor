a228a43cbf9a02b7baf904095d65338a82687dfe
kuanysh0799 <Kuanysh.janabaev@icloud.com>
2025-09-06 06:38:59 +0500

    Add files via upload

---
+++ firebase-config.js
@@ -0,0 +1,9 @@
+// Вставьте ваши реальные ключи из Firebase Console → Project settings → Web app
+export const firebaseConfig = {
+  apiKey: "PASTE_API_KEY",
+  authDomain: "PASTE_PROJECT_ID.firebaseapp.com",
+  projectId: "PASTE_PROJECT_ID",
+  storageBucket: "PASTE_PROJECT_ID.appspot.com",
+  messagingSenderId: "PASTE_SENDER_ID",
+  appId: "PASTE_APP_ID"
+};
