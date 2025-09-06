e65a95145348ab8afc67ed8f25356b11c5c27582
kuanysh0799 <Kuanysh.janabaev@icloud.com>
2025-09-06 14:33:33 +0500

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,12 +1,6 @@
-// sw.js
 const CACHE='opendoor-stable-v1';
 const ASSETS=[
-  './',
-  './index.html',
-  './styles.css?v=st1',
-  './app.js?v=st1',
-  './manifest.webmanifest',
-  './apple-touch-icon.png',
-  './icon-192.png',
-  './icon-512.png'
+  './','./index.html','./styles.css?v=st1','./app.js?v=st1',
+  './manifest.webmanifest','./apple-touch-icon.png',
+  './icon-192.png','./icon-512.png' // <-- было ./assets/… (ошибка путей)
 ];
