4354242c5c7e91dd6d550848932fd581833c102c
kuanysh0799 <Kuanysh.janabaev@icloud.com>
2025-09-06 13:50:19 +0500

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -1,9 +1,10 @@
+// firebase-config.js
 export const firebaseConfig = {
-  apiKey: "AIzaSyDFGSt9lWNg-SmTSuI4Af39XZBNjL9yTIY",
+  apiKey: "…",
   authDomain: "opendoor-85e8d.firebaseapp.com",
   projectId: "opendoor-85e8d",
-  storageBucket: "opendoor-85e8d.appspot.com",
+  storageBucket: "opendoor-85e8d.firebasestorage.app",
   messagingSenderId: "212873682532",
-  appId: "1:212873682532:web:d4a9f89a596faffced4971",
-  measurementId: "G-4D57SWBCF5"
+  appId: "1:212873682532:web:…",
+  measurementId: "G-…"
 };
