10ae10872acc6d27789c503bb2284b74d42578a5
kuanysh0799 <Kuanysh.janabaev@icloud.com>
2025-09-06 12:39:51 +0500

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,16 +1,1 @@
-const CACHE='od-v15';
-self.addEventListener('install',e=>{
-  self.skipWaiting();
-  e.waitUntil(caches.open(CACHE).then(c=>c.addAll([
-    './','./index.html','./styles.css?v=15','./app.js?v=15','./firebase-config.js?v=15','./manifest.webmanifest'
-  ])));
-});
-self.addEventListener('activate',e=>{
-  e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))));
-  self.clients.claim();
-});
-self.addEventListener('fetch',e=>{
-  e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request).then(res=>{
-    const copy=res.clone(); caches.open(CACHE).then(c=>c.put(e.request,copy)); return res;
-  })));
-});
+self.addEventListener('fetch',()=>{});
